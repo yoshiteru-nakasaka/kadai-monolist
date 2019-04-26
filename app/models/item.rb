7c978b50087e0457defa1aff0cb01ea0979dcb2a
yoshiteru.nakasaka <yoshiteru0118@gmail.com>
2019-04-26 06:00:28 +0000

    want

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :url, presence: true, length: { maximum: 255 }
   validates :image_url, presence: true, length: {maximum: 255 }
+  
+  has_many :wants
+  has_many :want_users, through: :wants, source: :user
 end
